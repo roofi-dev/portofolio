3757bc92eabf8b0eda6f6635ecffb77b0db108a0
Roofi Ladang Lima <roofi.ladanglima@gmail.com>
2025-08-12 01:52:52 +0000

    terdapat kendala saat saya coba upload ke vercel melalui github yaitu [

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import { type NextConfig } from 'next';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  // basePath and assetPrefix are used for deploying to a sub-path on a domain.
-  // This is useful for services like GitHub Pages.
-  // The value should be the name of your repository.
-  basePath: isProd ? '/portofolio' : '',
-  assetPrefix: isProd ? '/portofolio/' : '',
   images: {
-    // Disabling image optimization is required for `next export`.
-    // If you are not using `next export`, you can remove this.
     unoptimized: true,
   },
 };
